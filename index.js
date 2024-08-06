e87a3642f4c5c45f102bec60d93db19be53258e7
Yash121l <yashlunawat121@gmail.com>
2024-08-06 15:27:36 +0530

    "Refactor logging functions to use fs.appendFileSync instead of fs.writeFile; update logInfo call"

--- index.js
+++ index.js
@@ -11,32 +11,15 @@ const fs = require('fs');
 
 
 function logInfo(message) {
-  const content = new Date().toISOString() + " | " + "INFO" + " | " + message
-  fs.writeFile('./logs/app.log', content, err => {
-    if (err) {
-    } else {
-    }
-  })
+  fs.appendFileSync( './logs/app.log' , new Date().toISOString() + " | " + "INFO" + " | " + message +"\n")
 }
 
 function logWarning(message) {
-  const content = new Date().toISOString() + " | " + "WARN" + " | " + message
-  fs.writeFile('./logs/app.log', content, err => {
-    if (err) {
-    } else {
-    }
-  })
+  fs.appendFileSync( './logs/app.log' , new Date().toISOString() + " | " + "WARN" + " | " + message +"\n")
 }
 
-
-
 function logError(message) {
-  const content = new Date().toISOString() + " | " + "ERROR" + " | " + message
-  fs.writeFile('./logs/app.log', content, err => {
-    if (err) {
-    } else {
-    }
-  })
+  fs.appendFileSync( './logs/app.log' , new Date().toISOString() + " | " + "ERROR" + " | " + message +"\n")
 }
 
 logInfo("This is an info message");
